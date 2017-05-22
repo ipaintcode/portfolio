ccbc8f6f15296c2f4a8469fe8b9094503b18b8cc
Grayson Gilmore <gilmoreg@live.com>
2017-05-22 18:29:23 -0500

    Skills badges

--- src/components/Preview/Repos/Condictor.js
+++ src/components/Preview/Repos/Condictor.js
@@ -6,7 +6,7 @@ import './Condictor.css';
 
 const Condictor = props => (
   <section className="Condictor">
-    <img src="http://res.cloudinary.com/dk85nueap/image/upload/v1494963896/condictor_montage_udqqmv.jpg" alt="Condictor" />
+    <img src="https://cloud.githubusercontent.com/assets/18176333/26330669/b2e90af6-3f12-11e7-96a3-b5349ef61d8f.png" alt="Condictor" />
     <h1>Condictor</h1>
     <p>As this was my final project as a <strong>Thinkful</strong> student, I really wanted to shoot for the moon on this one. I challenged myself to pick up several frameworks and libraries outside the curriculum and broaden my knowledge as much as possible.</p>
     <p>I decided early that I would try out <strong>GraphQL</strong> and that turned out to be an amazing adventure of highs and lows! The server side implementation was unexpectedly easy; the challenges really came on the client side. I was drawn to <strong>Apollo Client</strong> and I tried hard to make it work, but ultimately decided that this project wasn’t a good fit. I learned a ton about <strong>Redux</strong> trying to make it play along with Apollo, and I experienced some of the pains of dealing with emerging technology (at one point discovering the source of a problem in a change that wasn’t documented outside of the changelog). In the end it made more sense for me to use <strong>Lokka</strong> in my Redux actions, but I was grateful for the experience.</p>
